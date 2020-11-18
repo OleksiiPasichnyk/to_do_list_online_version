0f099518a99de78ee6f5d612f86e780c4ff4d224
Oleksii Pasichnyk <axelpasechnik@gmail.com>
2020-11-18 09:05:02 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/to_do_list_online_version/precache-manifest.aebce3fb8afcc7ab990f10a9f38463d3.js"
+  "/to_do_list_online_versionv2/precache-manifest.20864ecb8f2744fd7a660f72d7be0e5f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/to_do_list_online_version/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/to_do_list_online_versionv2/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
