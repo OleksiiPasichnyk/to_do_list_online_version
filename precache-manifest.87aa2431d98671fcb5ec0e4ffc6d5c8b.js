ce9c87f3b26647bcddd119c214846443044858a4
Oleksii Pasichnyk <axelpasechnik@gmail.com>
2020-11-19 07:12:01 -0500

    Updates

---
+++ precache-manifest.87aa2431d98671fcb5ec0e4ffc6d5c8b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9537ac7d15e873c6bff21e8379e133e9",
+    "url": "/to_do_list_online_version/index.html"
+  },
+  {
+    "revision": "57f5ff85d70b6579dc81",
+    "url": "/to_do_list_online_version/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "68557a5d017a8ea6a575",
+    "url": "/to_do_list_online_version/static/js/2.bd988e33.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/to_do_list_online_version/static/js/2.bd988e33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57f5ff85d70b6579dc81",
+    "url": "/to_do_list_online_version/static/js/main.fdad988c.chunk.js"
+  },
+  {
+    "revision": "ea9294962cb9e638aa1d",
+    "url": "/to_do_list_online_version/static/js/runtime-main.3f875cdd.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/to_do_list_online_versionv2/precache-manifest.20864ecb8f2744fd7a660f72d7be0e5f.js"
+  "/to_do_list_online_version/precache-manifest.87aa2431d98671fcb5ec0e4ffc6d5c8b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/to_do_list_online_versionv2/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/to_do_list_online_version/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
